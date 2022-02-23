aa02f25bf37087e496a3f12bebd8425cd9a1f2c5
basalcode <basalcode@gmail.com>
2022-02-23 23:17:28 +0900

    Create project and add examples of data types and lists

---
+++ types.py
@@ -0,0 +1,24 @@
+# terminal
+print("Hello, World!")
+
+# data types
+a_string = "This is string type"
+a_number = 3
+a_float = 3.14
+a_boolean = True
+a_none = None
+
+# lists
+days = ["Mon", "Tue", "Wed", "Thur", "Fri", "Sat"]
+
+print("What type is this?:", type(days)) # type 구하기
+print("Does monday exist?:", "Mon" in days) # Mon 데이터가 있는지 확인
+print("What is in the second index?:", days[2]) # List의 요소 조회하기
+print("How long is the array?:", len(days)) # List의 길이 구하기
+
+days.append("Sun") # list에 요소 추가
+print("Sunday has been added!", days) 
+
+days.reverse() # list를 거꾸로 뒤집기
+print("Days list has been reversed!", days)
+
