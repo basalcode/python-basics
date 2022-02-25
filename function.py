d2580c7c00001088b3b8f846939ecb77dccdac4e
basalcode <basalcode@gmail.com>
2022-02-25 23:51:04 +0900

    Create examples of function

---
+++ function.py
@@ -0,0 +1,16 @@
+# function
+def say_hello():
+    print("hello world!")
+    print ("bye")
+
+say_hello()
+
+def say_hello_to(who = "nobody"):
+    print("Hello,", who)
+
+say_hello_to("Alex")
+
+def plus(a, b):
+    return a + b
+
+print(plus(1, 3))
