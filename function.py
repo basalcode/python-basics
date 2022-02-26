6dad4fee207a73c3ec7face0fa19e57f7fb3368e
basalcode <basalcode@gmail.com>
2022-02-26 17:34:27 +0900

    Create examples of function and string formatter

--- function.py
+++ function.py
@@ -13,4 +13,20 @@ say_hello_to("Alex")
 def plus(a, b):
     return a + b
 
-print(plus(1, 3))
+print("plus(1, 3)", plus(1, 3))
+
+def minus(a, b):
+    return a - b
+
+print("minus(3, 2)", minus(5, 2))
+print("minus(b = 3, a = 2)", minus(b = 5, a = 2)) # keyworded arguments 
+
+# string formatter
+def introduce(name, age):
+    print("Hello My name is " + name + ". I'm " + str(age) + " years old.")
+
+def introduceWithFormatter(name, age):
+    print(f"Hello My name is {name}. I'm {age} years old.")
+
+introduce("Alex", 20)
+introduceWithFormatter(age = 20, name = "Alex")
